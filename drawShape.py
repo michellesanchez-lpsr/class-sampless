8120cfa6ab12bc23a215938da69d65ad04a9e7c5
LPS Student <lps@lps-1011PX.(none)>
2016-01-05 15:36:15 -0800

    Classwork

---
+++ drawShape.py
@@ -0,0 +1,28 @@
+import turtle
+
+# this will draw ONE of the sides for our shape
+def drawSide(myTurtle):
+	count = 0
+	while count < 4:
+		drawVee(myTurtle)
+		count = count + 1
+
+def drawVee(myTurtle):
+	myTurtle.forward(10)
+	myTurtle.right(90)
+	myTurtle.forward(10)
+	myTurtle.left(90)
+
+shawn = turtle.Turtle()
+count = 0
+while count < 4:
+	drawSide(shawn)
+	shawn.right(90)
+	count = count + 1
+
+
+
+drawSide(shawn)
+
+
+turtle.exitonclick()
